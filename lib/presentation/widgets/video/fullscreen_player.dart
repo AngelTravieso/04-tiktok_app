15d4fe77d388cf12784d14df2dfa1ebfea2546a5
atravieso <angeltraviesoc@gmail.com>
2023-03-29 18:56:03 -0400

    gradiente de fondo

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_app/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -54,6 +55,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
                 VideoPlayer(controller),
 
                 // Gradiente
+                VideoBackground(
+                  stops: const [0.8, 1.0],
+                ),
 
                 // texto
                 Positioned(
